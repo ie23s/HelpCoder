ce3b8738b453b4928a56b2006b32ec42e5baa4f5
ie23s <evtukhov2222@gmail.com>
2019-12-05 21:05:26 +0200

    1

---
+++ app/src/main/java/com/ie23s/android/app/helpcoder/project/Project.java
@@ -0,0 +1,13 @@
+package com.ie23s.android.app.helpcoder.project;
+
+import java.util.UUID;
+
+public class Project {
+    private UUID uuid;
+    private String name;
+    private int maxWeight;
+    private int weight;
+
+    public Project() {
+    }
+}
