d3f1587f4a9d8bb9b19364511ed6a5fac15b7007
Ilya-Evtukhov <evtuhov2222@ya.ru>
2019-12-09 03:45:38 +0200

    1.1.47

--- app/git.sh
+++ app/git.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $1
 git add -A && git commit -m $2
-git push
+git push https://ie23s:Iil3764a@github.com/ie23s/HelpCoder.git
---
+++ app/git.sh~
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd $1
+git add -A && git commit -m $2
+git push
